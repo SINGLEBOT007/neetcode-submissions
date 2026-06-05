5c6cfaa689d0f21c2e33be973c6ce898f7cf11ab
SINGLEBOT007 <95434504+SINGLEBOT007@users.noreply.github.com>
2026-06-05 07:13:19 +0530

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        #not O(1) space
+        # counter_nums = Counter(nums)
+        # return max(counter_nums, key = counter_nums.get)
+
+        #not a linked list but treat list array as linked list to find dupes
+
+        slow, fast = 0,0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slower = 0
+
+        while True:
+            slow = nums[slow]
+            slower = nums[slower]
+            if slow == slower:
+                return slow
+        
